2b4c08a58a96d08c27c05a973272d9416e067e63
cristianoxxm <318539225@qq.com>
2022-03-05 05:23:27 +0800

    组员开启第一个分支
    Signed-off-by: cristianoxxm <318539225@qq.com>

---
+++ src/main/java/com/core/git/first/FirstModify.java
@@ -0,0 +1,14 @@
+package com.core.git.first;
+
+/**
+ * @author cristianoxiaoming@gmail.com
+ * @version 1.0
+ * @ClassName FirstModify
+ * @Package com.core.git.first
+ * @ProjectName git_idea_test_modify
+ * @description TODO
+ * @date 2022/03/05 5:20
+ * @ProductName IntelliJ IDEA
+ */
+public class FirstModify {
+}
